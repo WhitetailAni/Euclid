36fdef12bf9c9b5df4f8337f69decbbb41be9a47
RealKGB <realkgb@MacBook-Max.local>
2023-06-29 11:15:18 -0500

    Initial Commit

---
+++ Euclid/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Euclid
+//
+//  Created by RealKGB on 6/29/23.
+//  Copyright © 2023 RealKGB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
